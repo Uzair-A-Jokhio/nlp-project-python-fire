6986206f919ecd8a7034e08568f9611fb681c82d
Uzair <114494083+Uzair-A-Jokhio@users.noreply.github.com>
2025-02-26 18:40:41 +0000

    fixed some bugs: removed unnessary import from test_corenlp correctly naming actions XD

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,10 +1,7 @@
-from nlplogic.corenlp import get_phrases, search_wikipedia, get_text_blob
+from nlplogic.corenlp import get_phrases, search_wikipedia
 
 def test_get_phrases():
     assert 'islamabad' in get_phrases('pakistan')
 
 def test_search_wikipedia():
     assert 'Chinese' in search_wikipedia('china')
-
-def test_get_text_blob():
-    assert
